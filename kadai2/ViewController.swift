3c654b715b882a758870b3276904e65c7760d656
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2020-12-03 20:42:58 +0900

    インデント修正

--- kadai2/ViewController.swift
+++ kadai2/ViewController.swift
@@ -13,33 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet private var calculateSegmentedControl: UISegmentedControl!
     @IBOutlet private var calculateButton: UIButton!
     @IBOutlet private var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func calculateButton(_ sender: Any) {
-    let number1 = Double(number1TextField.text!) ?? 0
-    let number2 = Double(number2TextField.text!) ?? 0
-    var kekka :Double = 0
-
+        let number1 = Double(number1TextField.text!) ?? 0
+        let number2 = Double(number2TextField.text!) ?? 0
+        var kekka :Double = 0
+        
         switch calculateSegmentedControl.selectedSegmentIndex{
-    case 0:
-        kekka = number1 + number2
-    case 1:
-        kekka = number1 - number2
-    case 2:
-        kekka = number1 * number2
-    case 3:
-        guard number2 != 0 else{
-            answerLabel.text = "割る数には０以外の数字を入れてください"
-            return
+        case 0:
+            kekka = number1 + number2
+        case 1:
+            kekka = number1 - number2
+        case 2:
+            kekka = number1 * number2
+        case 3:
+            guard number2 != 0 else{
+                answerLabel.text = "割る数には０以外の数字を入れてください"
+                return
+            }
+            kekka = number1 / number2
+            
+        default:
+            print("該当なし")
         }
-        kekka = number1 / number2
-
-      default:
-        print("該当なし")
-    }
         answerLabel.text = String(kekka)
     }
     
